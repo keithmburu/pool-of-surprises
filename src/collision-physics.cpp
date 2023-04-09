eb4df068c9266fd09743008673c646091ca4c627
kmburu <keithmburu1@gmail.com>
2023-04-09 13:26:30 -0400

    trajectory estimation viz, launching in 3D

--- src/collision-physics.cpp
+++ src/collision-physics.cpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include "agl/window.h"
 #include "unistd.h"
+#include <ctime>
 
 using namespace std;
 using namespace glm;
@@ -29,19 +30,19 @@ public:
   Viewer() : Window() {
     _width = _height = 500;
     _radius = _viewVolumeSide;
-    srand(elapsedTime());
   }
 
   void setup() {
     setWindowSize(_width, _height);
+    // srand(time(nullptr));
     createBalls();
     renderer.setDepthTest(false);
-    renderer.blendMode(agl::DEFAULT);
     renderer.loadTexture("ball", "../textures/white-circle.png", 0);
+    renderer.loadTexture("trajectoryBall", "../textures/ParticleBokeh.png", 0);
     renderer.loadShader("phong-pixel", "../shaders/phong-pixel.vs", "../shaders/phong-pixel.fs");
   }
 
-void createBalls()
+  void createBalls()
   {
     for (int i = 0; i < _numBalls; i++) {
       Ball ball;
@@ -59,7 +60,7 @@ void createBalls()
     }
   }
 
- void updateBalls()
+  void updateBalls()
   {
     std::vector<Ball> newBalls = _Balls;
     for (int i = 0; i < _numBalls; i++) {
@@ -67,20 +68,21 @@ void createBalls()
       Ball newBall = newBalls[i];
       for (int j = 0; j < _numBalls; j++) {
         Ball otherBall = _Balls[j];
-        if (ball.id != otherBall.id && length(ball.pos - otherBall.pos) <= 3.5) {
+        if (ball.id != otherBall.id && length(ball.pos - otherBall.pos) <= 50) {
           newBall.vel = (otherBall.vel - ball.vel) / 2.0f;
           break;
         } 
       }
       newBall.pos += newBall.vel * dt();
-      if (newBall.pos.x < -250 || newBall.pos.x > 250) {
-        newBall.pos.x = (newBall.pos.x < -250)? -250 : newBall.pos.x;
-        newBall.pos.x = (newBall.pos.x > 250)? 250 : newBall.pos.x;
+      int thresh = _viewVolumeSide / 2;
+      if (newBall.pos.x < -thresh || newBall.pos.x > thresh) {
+        newBall.pos.x = (newBall.pos.x < -thresh)? -thresh : newBall.pos.x;
+        newBall.pos.x = (newBall.pos.x > thresh)? thresh : newBall.pos.x;
         newBall.vel.x = -newBall.vel.x;
       }
-      if (newBall.pos.y < -250 || newBall.pos.y > 250) {
-        newBall.pos.y = (newBall.pos.y < -250)? -250 : newBall.pos.y;
-        newBall.pos.y = (newBall.pos.y > 250)? 250 : newBall.pos.y;
+      if (newBall.pos.y < -thresh || newBall.pos.y > thresh) {
+        newBall.pos.y = (newBall.pos.y < -thresh)? -thresh : newBall.pos.y;
+        newBall.pos.y = (newBall.pos.y > thresh)? thresh : newBall.pos.y;
         newBall.vel.y = -newBall.vel.y;
       }
       // cout << "pos: " << newBall.pos << "  vel: " << newBall.vel << endl;
@@ -89,7 +91,7 @@ void createBalls()
     _Balls = newBalls;
   }
 
- void drawBalls()
+  void drawBalls()
   {
     vec3 cameraPos = renderer.cameraPosition();
 
@@ -109,6 +111,8 @@ void createBalls()
 
     // draw
     renderer.texture("image", "ball");
+    renderer.blendMode(agl::DEFAULT);
+    renderer.beginShader("phong-pixel");
     for (int i = 0; i < _numBalls; i++)
     {
       // renderer.sprite(_Balls[i].pos, _Balls[i].color, _Balls[i].size, 0.0);
@@ -116,100 +120,122 @@ void createBalls()
       renderer.translate(_Balls[i].pos);
       renderer.scale(vec3(_Balls[i].size));
       // setting phong shading uniforms
-      setupReflections();
+      setupReflections(i);
       renderer.sphere();
       renderer.pop();
     }
+    renderer.endShader();
+
+    if (_launching) {
+      renderer.texture("image", "trajectoryBall");
+      renderer.blendMode(agl::ADD);
+      renderer.beginShader("sprite");
+      for (int i = 0; i < _trajectoryBalls.size(); i++)
+      {
+        renderer.sprite(_trajectoryBalls[i].pos, _trajectoryBalls[i].color, _trajectoryBalls[i].size, 0.0);
+      }
+      renderer.endShader();
+    }
   }
 
-void setupReflections() {
-  renderer.setUniform("ViewMatrix", renderer.viewMatrix());
-  renderer.setUniform("ProjMatrix", renderer.projectionMatrix());
-  renderer.setUniform("lightPos", _lightPos);
-  renderer.setUniform("lightColor", _lightColor.x, _lightColor.y, _lightColor.z);
-  renderer.setUniform("materialColor", vec4(1.0, 0, 0, 1.0));
-  renderer.setUniform("eyePos", _camPos);
-  float ka = 0.1, kd = 0.7, ks = 0.6;
-  float phongExp = 50.0;
-  renderer.setUniform("ka", ka);
-  renderer.setUniform("kd", kd);
-  renderer.setUniform("ks", ks);
-  renderer.setUniform("phongExp", phongExp);
-}
+  void setupReflections(int ballIdx) {
+    renderer.setUniform("ViewMatrix", renderer.viewMatrix());
+    renderer.setUniform("ProjMatrix", renderer.projectionMatrix());
+    renderer.setUniform("lightPos", _lightPos);
+    renderer.setUniform("lightColor", _lightColor.x, _lightColor.y, _lightColor.z);
+    renderer.setUniform("materialColor", _Balls[ballIdx].color);
+    renderer.setUniform("eyePos", _camPos);
+    float ka = 0.1, kd = 0.7, ks = 0.6;
+    float phongExp = 50.0;
+    renderer.setUniform("ka", ka);
+    renderer.setUniform("kd", kd);
+    renderer.setUniform("ks", ks);
+    renderer.setUniform("phongExp", phongExp);
+  }
 
-void mouseMotion(int x, int y, int dx, int dy) {
-      if (_leftClick) {
+  void mouseMotion(int x, int y, int dx, int dy) {
+    if (_leftClick) {
+      if (_launching) {
+        _launchVel += vec3(-dx, dy, 0);
+        // cout << _launchVel << endl;
+        for (int i = 0; i < _trajectoryBalls.size(); i++) {
+          _trajectoryBalls[i].pos = _Balls[_activeBall].pos + ((1.0f / (i+1)) * _launchVel);
+        }
+      } else if (!_orbiting) {
+        float closestDist = 99999999;
+        float closestDistIdx = -1;
+        for (int i = 0; i < _numBalls; i++) {
+          int clickX = x - (_viewVolumeSide / 2);
+          int clickY = -(y - (_viewVolumeSide / 2));
+          vec4 eyePos = renderer.viewMatrix() * vec4(_Balls[i].pos, 1.0);
+          vec2 screenPos = vec2(ePos.x, ePos.y);
+          // cout << pos << " vs " << clickX << " " << clickY << " dist: " << length(pos - vec2(clickX, clickY)) << endl;
+          if (length(pos - vec2(clickX, clickY)) < 30) {
+            cout << "threshold crossed" << endl;
+            float dist = length(pos - vec2(clickX, clickY));
+            if (dist < closestDist) {
+              closestDist = dist;
+              closestDistIdx = i;
+            }
+            _launching = true;
+          } 
+        }
         if (_launching) {
-          _launchVel += vec3(-dx, dy, 0);
+          _Balls[closestDistIdx].vel = vec3(0);
+          _Balls[closestDistIdx].color /= 2.0f;
+          _activeBall = closestDistIdx;
+          _launchVel = vec3(-dx, dy, 0);
           // cout << _launchVel << endl;
-        } else if (!_orbiting) {
-          float closestDist = 99999999;
-          float closestDistIdx = -1;
-          for (int i = 0; i < _numBalls; i++) {
-            // int clickX = (x / 10.0f) - 25;
-            // int clickY = -((y / 10.0f) - 25);
-            int clickX = x - (_viewVolumeSide / 2);
-            int clickY = -(y - (_viewVolumeSide / 2));
-            cout << _Balls[i].pos << " vs " << clickX << " " << clickY << "  diff: " << abs(_Balls[i].pos.x - clickX) << " " << abs(_Balls[i].pos.y - clickY) << " dist: " << length(_Balls[i].pos - vec3(clickX, clickY, 0)) << endl;
-            if (length(_Balls[i].pos - vec3(clickX, clickY, 0)) < 30) {
-              cout << "threshold crossed" << endl;
-              float dist = length(_Balls[i].pos - vec3(clickX, clickY, 0));
-              if (dist < closestDist) {
-                closestDist = dist;
-                closestDistIdx = i;
-              }
-              _launching = true;
-            } 
+          for (int i = 0; i < 5; i++) {
+            Ball trajectoryBall;
+            trajectoryBall.pos = _Balls[_activeBall].pos + ((1.0f / (i+1)) * _launchVel);
+            trajectoryBall.color = vec4(0.8);
+            trajectoryBall.size = 25.0;
+            _trajectoryBalls.push_back(trajectoryBall);
           }
-          if (_launching) {
-            _Balls[closestDistIdx].vel = vec3(0);
-            _Balls[closestDistIdx].color /= 2.0f;
-            _activeBall = closestDistIdx;
-            _launchVel = vec3(-dx, dy, 0);
-            // cout << _launchVel << endl;
-          } else {
-            // orbit
-            _orbiting = true;
-          }
-        }
-        if (_orbiting) {
-          float ONE_DEG = 0.017; // one degree in radians
-          _elevation += dy * (M_PI / 180);
-          if (_elevation > (M_PI / 2) - ONE_DEG) {
-             _elevation = (M_PI / 2) - ONE_DEG;
-          } else if (_elevation < -((M_PI / 2) - ONE_DEG)) {
-             _elevation = -((M_PI / 2) - ONE_DEG);
-          }
-          _azimuth -= dx * (M_PI / 180);
-          if (_azimuth > 2 * M_PI) {
-             _azimuth = 0;
-          } else if (_azimuth < 0) {
-             _azimuth = 2 * M_PI;
-          }
+        } else {
+          _orbiting = true;
         }
       }
-   }
+      if (_orbiting) {
+        float ONE_DEG = 0.017;
+        _elevation += dy * (M_PI / 180);
+        if (_elevation > (M_PI / 2) - ONE_DEG) {
+           _elevation = (M_PI / 2) - ONE_DEG;
+        } else if (_elevation < -((M_PI / 2) - ONE_DEG)) {
+           _elevation = -((M_PI / 2) - ONE_DEG);
+        }
+        _azimuth -= dx * (M_PI / 180);
+        if (_azimuth > 2 * M_PI) {
+           _azimuth = 0;
+        } else if (_azimuth < 0) {
+           _azimuth = 2 * M_PI;
+        }
+      }
+    }
+  }
 
-   void mouseDown(int button, int mods) {
-      if (button == GLFW_MOUSE_BUTTON_LEFT) {
-         _leftClick = true;
-         _leftClickTime = elapsedTime();
-      }
-   }
+  void mouseDown(int button, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+       _leftClick = true;
+       _leftClickTime = elapsedTime();
+    }
+  }
 
-   void mouseUp(int button, int mods) {
-      if (button == GLFW_MOUSE_BUTTON_LEFT) {
-        _leftClick = false;
-        if (_launching) {
-          // cout << "active ball: " << _activeBall << "  launchVel: " << _launchVel << endl;
-          _Balls[_activeBall].vel = _launchVel;
-          _Balls[_activeBall].color *= 2.0f;
-          _launching = false;
-        } else if (_orbiting) {
-          _orbiting = false;
-        }
+  void mouseUp(int button, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT) {
+      _leftClick = false;
+      if (_launching) {
+        // cout << "active ball: " << _activeBall << "  launchVel: " << _launchVel << endl;
+        _Balls[_activeBall].vel = _launchVel;
+        _Balls[_activeBall].color *= 2.0f;
+        _launching = false;
+        _trajectoryBalls.clear();
+      } else if (_orbiting) {
+        _orbiting = false;
       }
-   }
+    }
+  }
 
   void scroll(float dx, float dy) {
     // scroll up or down to zoom in or out
@@ -218,7 +244,7 @@ void mouseMotion(int x, int y, int dx, int dy) {
        _radius = 1;
     }
     // scroll sideways to slowly pan sideways
-    _azimuth += 10 * dx * (M_PI / 180);
+    _azimuth -= 10 * dx * (M_PI / 180);
     if (_azimuth > 2 * M_PI) {
        _azimuth = 0;
     } else if (_azimuth < 0) {
@@ -235,9 +261,6 @@ void mouseMotion(int x, int y, int dx, int dy) {
   }
 
   void draw() {
-    // renderer.beginShader("sprite");
-    renderer.beginShader("phong-pixel");
-
     float aspect = ((float)width()) / height();
     renderer.perspective(glm::radians(60.0f), aspect, 0.1f, _viewVolumeSide * 2);
 
@@ -255,8 +278,6 @@ void mouseMotion(int x, int y, int dx, int dy) {
     updateBalls();
     drawBalls();
 
-    renderer.endShader();
-    // renderer.endShader();
   }
 
 protected:
@@ -275,8 +296,10 @@ protected:
   bool _leftClick = false;
   float _leftClickTime;
   bool _launching = false;
+  std::vector<Ball> _trajectoryBalls;
   int _activeBall;
   vec3 _launchVel;
+
   vec3 _lightPos = vec3(_viewVolumeSide, _viewVolumeSide, _viewVolumeSide);
   vec3 _lightColor = vec3(1.0, 1.0, 1.0);
   float _azimuth = 0;
