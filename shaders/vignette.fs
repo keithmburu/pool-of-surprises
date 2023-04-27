712bc0a80f61216b5e4b361040a09c6dd25fc40d
kmburu <keithmburu1@gmail.com>
2023-04-27 00:57:22 -0400

    transition from white

--- shaders/vignette.fs
+++ shaders/vignette.fs
@@ -6,10 +6,16 @@ uniform vec2 Resolution;
 void main() {
     vec2 pos = (gl_FragCoord.xy / Resolution.xy) * 2.0 - 1.0;
     float distFromCenter = length(pos);
-    float alpha = smoothstep(0.0, 1.0, distFromCenter);
+    float alpha = 1.0;
     float intensity = 1.0;
-    if (Time > 6.0 && Time < 10.0) {
-        intensity = smoothstep(1.0, 0.0, (Time - 6.0) / 4.0f);
+    if (Time > 6.0) {
+        alpha = smoothstep(0.0, 1.0, distFromCenter);
+        if (Time < 7.0) {
+            alpha = mix(1.0, alpha, (Time - 6.0) / 1.0f);
+        }
+        if (Time < 8.0) {
+            intensity = smoothstep(1.0, 0.0, (Time - 6.0) / 2.0f);
+        }
     }
     vec3 color = alpha * intensity * vec3(1.0);
     gl_FragColor = vec4(color, 1.0);
